37400b81e1014e7c3a52ab8d22b8724a797975dc
reza <gholamhasanireza@gmail.com>
2025-10-15 18:39:54 +0330

    work for today

--- blog/admin.py
+++ blog/admin.py
@@ -3,20 +3,21 @@ from django.contrib import admin
 from . import models
 
 
-
-
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user',)
 
+
 @admin.register(models.Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title','show_image','author')
+    list_display = ('title', 'show_image', 'author')
     list_filter = ('title',)
-    search_fields = ('title','body')
+    search_fields = ('title', 'body')
 
+#hi
 admin.site.register(models.Like)
