aabd7dc3f7467964a03b0c90f455c3da5b8503a0
reza <gholamhasanireza@gmail.com>
2025-10-23 22:08:59 +0330

    work for today

--- stand_blog/settings.py
+++ stand_blog/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#comment
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
